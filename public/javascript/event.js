132d06ad13f7f4fcea95def33e5af12cbbefd726
ginnyang <ginnayng@gmail.com>
2018-07-21 14:40:33 +0900

    getUser/:username added

--- public/javascript/event.js
+++ public/javascript/event.js
@@ -1,7 +1,17 @@
 $(function(){
+  //ajax get. static json data 요청
   $('#readJson').click(function(){
     $.get('/list',function(data){
       $('#jsonResult').html(data);
     });
+  });
+  //ajax get. static user data 요청
+  $('#search').on('keyup',function(e){
+    if(e.keyCode===13){
+      var username = $(this).val();
+      $.get('/getUser/'+username,function(data){
+        $('#userResult').html(data.name);
+      });
+    }
   });
 });
--- router/main.js
+++ router/main.js
@@ -1,12 +1,21 @@
 module.exports = function(app,fs){
+  //default 화면
   app.get('/',function(req,res){
     res.render('index', {title:"My homepage", length:5});
   });
-  //get api. 조회
+  //get api.static json data 조회/반환
   app.get('/list',function(req,res){
     fs.readFile(__dirname+"/../data/"+"user.json",'utf8',function(err,data){
       if(err) throw err;
       res.send(data);
     });
   });
+  //get api.static user data 조회/반환
+  app.get('/getUser/:username',function(req,res){
+    fs.readFile(__dirname+"/../data/"+"user.json",'utf8',function(err,data){
+      var users = JSON.parse(data);
+      res.json(users[req.params.username])
+    });
+  });
+  
 }
